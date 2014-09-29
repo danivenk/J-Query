5fd52bfd347f10baea82772c74d1adaeb29c8a54
Dani van Enk <danivenk@gmail.com>
2014-09-29 12:02:55 +0200

    css added in

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
 	<title>
 		J-Query
 	</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
 	<div class="container">
+		<div class="content">
+		Lorem ipsum dolor sit amet...
+		</div>
 	</div>
 </body>
 </html>
