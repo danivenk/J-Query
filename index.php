17db4a09fc9ec61f1d57da6553a7ce982ee3353d
Dani van Enk <danivenk@gmail.com>
2014-09-29 14:45:25 +0200

    content div del

--- index.php
+++ index.php
@@ -2,24 +2,22 @@
 <html>
 <head>
 	<title>
-		J-Query
+		jQuery
 	</title>
 	<link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
 	<div class="container">
-		<div class="content">
-			<div class="nav">
-				<ul class="nav">
-					<li class="nav"><a href="#">Unit 1</a></li>
-					<li class="nav"><a href="#">Unit 2</a></li>
-					<li class="nav"><a href="#">Unit 3</a></li>
-					<li class="nav"><a href="#">Unit 4</a></li>
-					<li class="nav"><a href="#">Unit 5</a></li>
-				</ul>
-			</div>
-		<h1>J-Query for Beginners</h1>
+		<div class="nav">
+			<ul class="nav">
+				<li class="nav"><a href="#">Unit 1</a></li>
+				<li class="nav"><a href="#">Unit 2</a></li>
+				<li class="nav"><a href="#">Unit 3</a></li>
+				<li class="nav"><a href="#">Unit 4</a></li>
+				<li class="nav"><a href="#">Unit 5</a></li>
+			</ul>
 		</div>
+	<h1>jQuery for Beginners</h1>
 	</div>
 </body>
 </html>
