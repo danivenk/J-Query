5faf3f0ad537b1108811860ca94656c9673965df
Dani van Enk <danivenk@gmail.com>
2014-09-29 14:37:30 +0200

    extra stuuf

--- index.php
+++ index.php
@@ -9,7 +9,16 @@
 <body>
 	<div class="container">
 		<div class="content">
-		Lorem ipsum dolor sit amet...
+			<div class="nav">
+				<ul class="nav">
+					<li class="nav"><a href="#">Unit 1</a></li>
+					<li class="nav"><a href="#">Unit 2</a></li>
+					<li class="nav"><a href="#">Unit 3</a></li>
+					<li class="nav"><a href="#">Unit 4</a></li>
+					<li class="nav"><a href="#">Unit 5</a></li>
+				</ul>
+			</div>
+		<h1>J-Query for Beginners</h1>
 		</div>
 	</div>
 </body>
