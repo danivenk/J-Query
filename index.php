857daf3097b79115cd90dae35187763948f89f9a
Dani van Enk <danivenk@gmail.com>
2014-09-29 16:24:41 +0200

    Kwak Kwak
    Tok Tok

--- index.php
+++ index.php
@@ -8,16 +8,18 @@
 </head>
 <body>
 	<div class="container">
-		<div class="nav">
-			<ul class="nav">
-				<li class="nav"><a href="#">Unit 1</a></li>
-				<li class="nav"><a href="#">Unit 2</a></li>
-				<li class="nav"><a href="#">Unit 3</a></li>
-				<li class="nav"><a href="#">Unit 4</a></li>
-				<li class="nav"><a href="#">Unit 5</a></li>
-			</ul>
-		</div>
-	<h1>jQuery for Beginners</h1>
+		<header>
+			<div class="nav">
+				<ul class="nav">
+					<li class="nav"><a href="#">Unit 1</a></li>
+					<li class="nav"><a href="#">Unit 2</a></li>
+					<li class="nav"><a href="#">Unit 3</a></li>
+					<li class="nav"><a href="#">Unit 4</a></li>
+					<li class="nav"><a href="#">Unit 5</a></li>
+				</ul>
+			</div>
+			<h1>jQuery for Beginners</h1>
+		</header>
 	</div>
 </body>
 </html>
