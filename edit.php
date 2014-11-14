203471d0be4c000707509c4b5ddd4033ea4dd62f
Mátyás Tóth <azhnarton@gmail.com>
2014-11-14 12:10:27 +0100

    Summary
    Description

---
+++ edit.php
@@ -0,0 +1,25 @@
+<!DoCTyPE html>
+<html>
+<head>
+	<title>TokEdit</title>
+	<link rel="stylesheet" type="text/css" href="css/editor.css">
+	<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+	
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	
+	<script>
+	</script>
+</head
+<body>
+	
+	<div class="editor">
+		<textarea></textarea>
+		<input type="button" value="Remove all"></input>
+	</div>
+	
+	
+	<div class="preview"></div>
+
+</body>
+</html>
