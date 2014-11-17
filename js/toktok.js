99345d5bd752a08529be0b2fc889d7b6543b652c
Dani van Enk <danivenk@gmail.com>
2014-11-17 13:52:40 +0100

    Checkup

--- js/toktok.js
+++ js/toktok.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-	var unitCount = 5;
+	var unitCount = $('.exp').length;
 	var selectedID = false;
 	
 	for (var i = 0; i < unitCount; i++) {
-		$('.nav').append(' |<a href="#u' + (i+1) + '">&nbsp;&nbsp;Unit ' + (i+1) + '</a>');
+		if (i == (unitCount - 1)) {
+			$('.nav').append(' |<a href="#u' + (i+1) + '">&nbsp;&nbsp;Unit ' + (i+1) + '</a> |');
+		} else {
+			$('.nav').append(' |<a href="#u' + (i+1) + '">&nbsp;&nbsp;Unit ' + (i+1) + '</a>');
+		}
 	}
 	
 	$('select option').click(function() {
